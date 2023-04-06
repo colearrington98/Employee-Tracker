c9c9a25c1504ed13ba68d2fc83319b1b099ecc59
Cole Arrington <119268105+colearrington98@users.noreply.github.com>
2023-04-05 22:05:06 -0500

    Debugging

--- db/connection.js
+++ db/connection.js
@@ -1,14 +1,17 @@
-const mysql = require("mysql");
+const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
     host: "localhost",
     user: "root",
-    password: "Vargvonburgly98",
-    database: "mydb"
+    password: "Vargvonburgly98$",
+    database: "employees"
 });
 
 connection.connect(function(err) {
-    if (err) throw err;
+    if (err) {
+        console.error('Error connecting: ' + err.stack);
+        process.exit(1);
+    }
     console.log("Connected!");
 });
 
